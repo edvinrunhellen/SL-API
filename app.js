97a84c230750aef6e32dd7445adffc0686c62d60
Eddan <edvin.runhellen1@gmail.com>
2025-01-12 10:45:52 +0100

    Added middleware authenticate function

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const app = express()
 
 //Middlewares
 app.use(express.json())
+app.use(authenticate)
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+function authenticate(req, res, next) {
+    const apiKey = req.headers['x-api-key'];
+    if (!apiKey) {
+        return res.status(401).send('Unauthorized');
+    }
+
+
+    if (apiKeyDB.includes(apiKey)) {
+        next()
+    } else {
+        return res.status(401).send('Unauthorized')
+    }
+}
+const apiKeyDB = ["APIKEY1", "APIKEY2"]
+
+app.get('/', authenticate, function (req, res) {
+    res.send('Hello Edvin')
 })
 
 const cardMockDB = [{
@@ -18,7 +34,7 @@ const cardMockDB = [{
 }
 ]
 
-app.get('/sl-access/:cardId', function (req, res) {
+app.get('/sl-access/:cardId', authenticate, function (req, res) {
     const cardId = req.params.cardId;
     const cardMatch = cardMockDB.filter(cardFromDB => cardFromDB.id == cardId)
     const cardActiveUntil = cardMatch[0].activeUntil;
@@ -33,7 +49,7 @@ const trainMockDB = [{
 }]
 
 
-app.post('/sl-departures', (req, res) => {
+app.post('/sl-departures', authenticate, (req, res) => {
     if (req.body.from == "Tekniska högskolan" && req.body.to == "Tcentralen") {
         res.send(trainMockDB)
     } else
